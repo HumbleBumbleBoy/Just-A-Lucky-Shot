8478d758658336df2d8d425474f70f5e4b37af91
Humble Bumble <157408786+HumbleBumbleBoy@users.noreply.github.com>
2026-04-15 19:39:42 +0200

    testing audio settings

--- UI/NavigationalButton/NavigationalButton.cs
+++ UI/NavigationalButton/NavigationalButton.cs
@@ -6,6 +6,7 @@ public partial class NavigationalButton : TextureButton
 {
     [Export] public string Text;
     [Export] public string Path;
+    [Export] public AudioStreamPlayer AudioStreamPlayer;
 
     private RichTextLabel textContainer;
 
@@ -15,8 +16,11 @@ public partial class NavigationalButton : TextureButton
         updateButtonText();
     }
 
-    private void onPressed()
+    private async void onPressed()
     {
+        AudioStreamPlayer.Play();
+        await ToSignal(GetTree().CreateTimer(0.1f), SceneTreeTimer.SignalName.Timeout); // wait for sfx to load
+
         if (Path == "Quit") { GetTree().Quit(); }
         
         if (!string.IsNullOrEmpty(Path))
